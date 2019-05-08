d9541a5ed7b110d916abe3ad0d266f8c02491188
feronimus <thelord_777_999@hotmail.com>
2019-05-08 14:52:27 +0300

    Mqtt 0.2

--- MqttServer/mqtt_handler.js
+++ MqttServer/mqtt_handler.js
@@ -1,4 +1,14 @@
 const mqtt = require('mqtt');
+const express = require('express');
+const router =  express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User =  require('../models/user');
+const Esp =  require('../models/esp');
+const multer = require('multer');
+
+
 
 class MqttHandler {
   constructor() {
@@ -9,9 +19,9 @@ class MqttHandler {
   }
   
   connect() {
-    // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
-    //this.mqttClient = mqtt.connect(this.host, { username: this.username, password: this.password });
+    
     this.mqttClient = mqtt.connect(this.host);
+
 
     // Mqtt error calback
     this.mqttClient.on('error', (err) => {
@@ -22,25 +32,58 @@ class MqttHandler {
     // Connection callback
     this.mqttClient.on('connect', () => {
       console.log(`mqtt client connected`);
-    });
+    }); 
 
-    // mqtt subscriptions
-    this.mqttClient.subscribe('esp/12', {qos: 0});
+    //subscribe to all existing esps
+    console.log("i run bastard");
+
 
     // When a message arrives, console.log it
-    this.mqttClient.on('message', function (topic, message) {
-      console.log(message.toString());
-    });
+    this.mqttClient.on('message', function(topic, message, packet) {
+        messageFunction(topic);
+        console.log("From : " +topic.substr(4) + "  - message : " + message );
+      });
+    function messageFunction(topic){
+        //topic esp/secret 
+        secret = topic.substr(4);
+
+        Esp.getEspBySecret(secret, (err, esp) =>{
+            if(err) throw err;
+            if(!esp){
+                return;
+            }  
+            var d = new Date();
+            var t= d.getTime()/ 1000 ;
+            esp.isOnline = t ;// how many seconds have passed since 1970/01/01
+            Esp.updateEsp(esp, (err, esp) => {});            
+        })
+        //prob reset all var ?????
+    };
 
     this.mqttClient.on('close', () => {
       console.log(`mqtt client disconnected`);
     });
   }
 
-  // Sends a mqtt message to topic: mytopic
-  sendMessage(message) {
-    this.mqttClient.publish('mytopic', message);
+  // Sends a mqtt message to topic
+  sendMessage(topic,message) {
+    this.mqttClient.publish(topic, message);
   }
+  
 }
 
+
+// mqtt subscriptions
+router.post('/subscribe', passport.authenticate('jwt' , {session:false}), (req, res, next) => {    
+    this.mqttClient.subscribe(req.body.subscription, {qos: 1});
+
+ });  
+// mqtt unsubscriptions
+router.post('/unsubscribe', passport.authenticate('jwt' , {session:false}), (req, res, next) => {
+
+    this.mqttClient.unsubscribe(req.body.unsubscription);
+
+ });     
+
+ 
 module.exports = MqttHandler;
